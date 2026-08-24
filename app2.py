4c4b11b659c77a36e889328c8eae63c62552da9f
mohamed <mohamed0124938@gmail.com>
2026-08-24 22:14:09 +0300

    Initial commit

---
+++ app2.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import joblib 
+import re 
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+@st.cache_resource
+def load_artifacts():
+    svm_model = joblib.load('svm_ticket_model.pkl')
+    priority_model = joblib.load('priority_model.pkl')
+    tf_idf = joblib.load('tfidf_vectorizer.pkl')
+    X_train_tf_idf = joblib.load('X_train_tf_idf.pkl')
+    X_train = joblib.load('X_train.pkl')
+    y_train = joblib.load('y_train.pkl')
+    return svm_model, priority_model, tf_idf, X_train_tf_idf, X_train, y_train
+
+svm_model, priority_model, tf_idf, X_train_tf_idf, X_train, y_train = load_artifacts()
+
+def clean_text(text):
+    text =text.lower()
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    words = text.split()
+    return " ".join([w for w in words if len(w) >2])
+
+st.set_page_config(page_title = 'Ticket Classification System', page_icon='*')
+st.title("Customer support ticket classifier")
+st.write("Enter the text of ticket to classify the problem, predict priority, define similarity tickets")
+
+user_input = st.text_area("Customer Message:",  "I was charged twice on my credit card for the same order, please refund me.")
+if st.button("Analyze Ticket"):
+    if user_input.strip():
+        cleaned = clean_text(user_input)
+        vec_input = tf_idf.transform([cleaned])
+
+        pred_category = svm_model.predict(vec_input)[0]
+        pred_priority = priority_model.predict(vec_input)[0]
+
+        col1, col2 = st.columns(2)
+        with col1:
+            st.success(f"**Predicted Category:** {pred_category}")
+        with col2:
+            st.warning(f"**Predicted priority:** {pred_priority}")
+        
+        similarities = cosine_similarity(vec_input, X_train_tf_idf).flatten()
+        top_indices = similarities.argsort()[-3:][::-1]
+        
+        st.subheader("Top 3 Similar Past Tickets:")
+        for i, idx in enumerate(top_indices, 1):
+            with st.expander(f"{i}. Category: {y_train.iloc[idx]} (Similarity Score: {similarities[idx]:.4f})"):
+                st.write(X_train.iloc[idx])
+    else:
+        st.error("الرجاء كتابة نص التذكرة أولاً.")
